b266658d4aa0fa5a49f985362b5d1166b46bae0e
Zoa Buske <zoa.buske@quantumretail.com>
2015-10-13 09:40:20 -0500

    made ws params PathParams rather than QueryParams

--- src/main/java/com/zbuske/productservicepoc/service/ProductService.java
+++ src/main/java/com/zbuske/productservicepoc/service/ProductService.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
@@ -21,9 +22,9 @@ public class ProductService {
 	}
 
 	@GET
-	@Path("byId")
+	@Path("byId/{id}")
 	@Produces(MediaType.TEXT_PLAIN)
-	public String getById(@QueryParam("id") String id) {
+	public String getById(@PathParam("id") String id) {
 		Integer intId = null;
 		try {
 			intId = Integer.parseInt(id.trim());
@@ -38,9 +39,9 @@ public class ProductService {
 	}
 
 	@GET
-	@Path("byIds")
+	@Path("byIds/{ids}")
 	@Produces(MediaType.TEXT_PLAIN)
-	public String getByIds(@QueryParam("ids") String ids) {
+	public String getByIds(@PathParam("ids") String ids) {
 		List<Integer> intIds = new ArrayList<Integer>();
 		String[] stringIds = ids.split(",");
 		for (int i = 0; i < stringIds.length; i++) {
@@ -59,9 +60,9 @@ public class ProductService {
 	}
 
 	@GET
-	@Path("byCategory")
+	@Path("byCategory/{category}")
 	@Produces(MediaType.TEXT_PLAIN)
-	public String getByCategory(@QueryParam("category") String category) {
+	public String getByCategory(@PathParam("category") String category) {
 		List<Product> products = productDao.selectByCategory(category);
 		if (products != null && !products.isEmpty()) {
 			return products.toString();
